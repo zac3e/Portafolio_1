ffa5d5658be20f49e4b2c1a972465fb37f5f038a
zac3e <diegomendez957@gmail.com>
2024-06-30 18:26:34 -0600

    .

--- Web/QuantaGuard/Home/urls.py
+++ Web/QuantaGuard/Home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='HOME'),
+    path('living/', views.living, name='living')
 ]
--- Web/QuantaGuard/Home/views.py
+++ Web/QuantaGuard/Home/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'Home.html')
+
+def living(request):
+    return render(request, 'Living.html')
+    
