3963cd654aa33753f27a69791844039dcba4129c
zac3e <diegomendez957@gmail.com>
2024-08-10 15:41:54 -0600

    .

--- Web/QuantaGuard/Home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='HOME'),
-    path('living/', views.living, name='living')
-]
--- Web/QuantaGuard/Home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'Home.html')
-
-def living(request):
-    return render(request, 'Living.html')
-    
